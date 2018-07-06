fe1e004736a3c22032c7af38bbc1feef1c29a0a3
Black Chopper <blackchopper@qq.com>
2018-07-06 22:32:26 +0800

    ImagePicker

---
+++ app/src/main/java/com/blackchopper/imagepicker/MainActivity.java
@@ -0,0 +1,175 @@
+package com.blackchopper.imagepicker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.blackchopper.imagepicker.bean.ImageItem;
+import com.blackchopper.imagepicker.ui.ImageGridActivity;
+import com.blackchopper.imagepicker.view.CropImageView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener, CompoundButton.OnCheckedChangeListener {
+    private ImagePicker imagePicker;
+
+    private RadioButton rb_glide;
+    private RadioButton rb_single_select;
+    private RadioButton rb_muti_select;
+    private RadioButton rb_crop_square;
+    private RadioButton rb_crop_circle;
+    private TextView tv_select_limit;
+
+    private EditText et_crop_width;
+    private EditText et_crop_height;
+    private EditText et_crop_radius;
+    private EditText et_outputx;
+    private EditText et_outputy;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imagePicker = ImagePicker.getInstance();
+        imagePicker.setImageLoader(new GlideImageLoader());
+
+        rb_glide = (RadioButton) findViewById(R.id.rb_glide);
+        rb_single_select = (RadioButton) findViewById(R.id.rb_single_select);
+        rb_muti_select = (RadioButton) findViewById(R.id.rb_muti_select);
+        rb_crop_square = (RadioButton) findViewById(R.id.rb_crop_square);
+        rb_crop_circle = (RadioButton) findViewById(R.id.rb_crop_circle);
+        rb_glide.setChecked(true);
+        rb_muti_select.setChecked(true);
+        rb_crop_square.setChecked(true);
+
+        et_crop_width = (EditText) findViewById(R.id.et_crop_width);
+        et_crop_width.setText("280");
+        et_crop_height = (EditText) findViewById(R.id.et_crop_height);
+        et_crop_height.setText("280");
+        et_crop_radius = (EditText) findViewById(R.id.et_crop_radius);
+        et_crop_radius.setText("140");
+        et_outputx = (EditText) findViewById(R.id.et_outputx);
+        et_outputx.setText("800");
+        et_outputy = (EditText) findViewById(R.id.et_outputy);
+        et_outputy.setText("800");
+
+        tv_select_limit = (TextView) findViewById(R.id.tv_select_limit);
+        SeekBar sb_select_limit = (SeekBar) findViewById(R.id.sb_select_limit);
+        sb_select_limit.setMax(15);
+        sb_select_limit.setOnSeekBarChangeListener(this);
+        sb_select_limit.setProgress(9);
+
+        CheckBox cb_show_camera = (CheckBox) findViewById(R.id.cb_show_camera);
+        cb_show_camera.setOnCheckedChangeListener(this);
+        cb_show_camera.setChecked(true);
+        CheckBox cb_crop = (CheckBox) findViewById(R.id.cb_crop);
+        cb_crop.setOnCheckedChangeListener(this);
+        cb_crop.setChecked(true);
+        CheckBox cb_isSaveRectangle = (CheckBox) findViewById(R.id.cb_isSaveRectangle);
+        cb_isSaveRectangle.setOnCheckedChangeListener(this);
+        cb_isSaveRectangle.setChecked(true);
+
+        Button btn_open_gallery = (Button) findViewById(R.id.btn_open_gallery);
+        btn_open_gallery.setOnClickListener(this);
+        findViewById(R.id.btn_open_camera).setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_open_gallery:
+                if (rb_single_select.isChecked()) imagePicker.setMultiMode(false);
+                else if (rb_muti_select.isChecked()) imagePicker.setMultiMode(true);
+
+                if (rb_crop_square.isChecked()) {
+                    imagePicker.setStyle(CropImageView.Style.RECTANGLE);
+                    Integer width = Integer.valueOf(et_crop_width.getText().toString());
+                    Integer height = Integer.valueOf(et_crop_height.getText().toString());
+                    width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, width, getResources().getDisplayMetrics());
+                    height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, height, getResources().getDisplayMetrics());
+                    imagePicker.setFocusWidth(width);
+                    imagePicker.setFocusHeight(height);
+                } else if (rb_crop_circle.isChecked()) {
+                    imagePicker.setStyle(CropImageView.Style.CIRCLE);
+                    Integer radius = Integer.valueOf(et_crop_radius.getText().toString());
+                    radius = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, radius, getResources().getDisplayMetrics());
+                    imagePicker.setFocusWidth(radius * 2);
+                    imagePicker.setFocusHeight(radius * 2);
+                }
+
+                imagePicker.setOutPutX(Integer.valueOf(et_outputx.getText().toString()));
+                imagePicker.setOutPutY(Integer.valueOf(et_outputy.getText().toString()));
+
+                Intent intent = new Intent(this, ImageGridActivity.class);
+                intent.putParcelableArrayListExtra(ImageGridActivity.EXTRAS_IMAGES, new ArrayList<ImageItem>());
+                //ImagePicker.getInstance().setSelectedImages(images);
+                startActivityForResult(intent, 100);
+                break;
+            case R.id.btn_open_camera:
+                ImagePicker.getInstance().setSelectLimit(1);
+                Intent intent1 = new Intent( this, ImageGridActivity.class);
+                intent1.putExtra(ImageGridActivity.EXTRAS_TAKE_PICKERS, true); // 是否是直接打开相机
+                startActivityForResult(intent1, 100);
+                break;
+        }
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        tv_select_limit.setText(String.valueOf(progress));
+        imagePicker.setSelectLimit(progress);
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        switch (buttonView.getId()) {
+            case R.id.cb_show_camera:
+                imagePicker.setShowCamera(isChecked);
+                break;
+            case R.id.cb_crop:
+                imagePicker.setCrop(isChecked);
+                break;
+            case R.id.cb_isSaveRectangle:
+                imagePicker.setSaveRectangle(isChecked);
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == ImagePicker.RESULT_CODE_ITEMS) {
+            if (data != null && requestCode == 100) {
+                Log.i("TAG", "onActivityResult: -----------------------");
+                ArrayList<ImageItem> images = data.getParcelableArrayListExtra(ImagePicker.EXTRA_RESULT_ITEMS);
+                Log.i("TAG", "onActivityResult: " + images.toString());
+
+            } else {
+                Toast.makeText(this, "没有数据", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
---
+++ library/src/main/java/com/blackchopper/imagepicker/adapter/ImagePageAdapter.java
@@ -0,0 +1,89 @@
+package com.blackchopper.imagepicker.adapter;
+
+import android.app.Activity;
+import android.support.v4.view.PagerAdapter;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.blackchopper.imagepicker.ImagePicker;
+import com.blackchopper.imagepicker.bean.ImageItem;
+import com.blackchopper.imagepicker.util.Utils;
+
+import java.util.ArrayList;
+
+import uk.co.senab.photoview.PhotoView;
+import uk.co.senab.photoview.PhotoViewAttacher;
+
+/**
+ * author  : Black Chopper
+ * e-mail  : 4884280@qq.com
+ * github  : http://github.com/BlackChopper
+ * project : ImagePicker
+ */
+public class ImagePageAdapter extends PagerAdapter {
+
+    private int screenWidth;
+    private int screenHeight;
+    private ImagePicker imagePicker;
+    private ArrayList<ImageItem> images = new ArrayList<>();
+    private Activity mActivity;
+    public PhotoViewClickListener listener;
+
+    public ImagePageAdapter(Activity activity, ArrayList<ImageItem> images) {
+        this.mActivity = activity;
+        this.images = images;
+
+        DisplayMetrics dm = Utils.getScreenPix(activity);
+        screenWidth = dm.widthPixels;
+        screenHeight = dm.heightPixels;
+        imagePicker = ImagePicker.getInstance();
+    }
+
+    public void setData(ArrayList<ImageItem> images) {
+        this.images = images;
+    }
+
+    public void setPhotoViewClickListener(PhotoViewClickListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        PhotoView photoView = new PhotoView(mActivity);
+        ImageItem imageItem = images.get(position);
+        imagePicker.getImageLoader().displayImagePreview(mActivity, imageItem.path, photoView, screenWidth, screenHeight);
+        photoView.setOnPhotoTapListener(new PhotoViewAttacher.OnPhotoTapListener() {
+            @Override
+            public void onPhotoTap(View view, float x, float y) {
+                if (listener != null) listener.OnPhotoTapListener(view, x, y);
+            }
+        });
+        container.addView(photoView);
+        return photoView;
+    }
+
+    @Override
+    public int getCount() {
+        return images.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+
+    @Override
+    public int getItemPosition(Object object) {
+        return POSITION_NONE;
+    }
+
+    public interface PhotoViewClickListener {
+        void OnPhotoTapListener(View view, float v, float v1);
+    }
+}
