bea51c6a770903f2cab40ba171b421c2330381b0
Black Chopper <blackchopper@qq.com>
2018-07-07 11:58:40 +0800

    Update ImagePreviewActivity.java

--- library/src/main/java/com/blackchopper/imagepicker/ui/ImagePreviewActivity.java
+++ library/src/main/java/com/blackchopper/imagepicker/ui/ImagePreviewActivity.java
@@ -1,222 +1,76 @@
 package com.blackchopper.imagepicker.ui;
 
-import android.content.Intent;
-import android.graphics.Color;
-import android.os.Bundle;
-import android.support.v4.view.ViewPager;
-import android.text.format.Formatter;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.Toast;
-
-import com.blackchopper.imagepicker.ImagePicker;
 import com.blackchopper.imagepicker.R;
-import com.blackchopper.imagepicker.bean.ImageItem;
-import com.blackchopper.imagepicker.util.NavigationBarChangeListener;
-import com.blackchopper.imagepicker.util.Utils;
-import com.blackchopper.imagepicker.view.SuperCheckBox;
 
 /**
- * ================================================
- * 作    者：jeasonlzy（廖子尧 Github地址：https://github.com/jeasonlzy0216
- * 版    本：1.0
- * 创建日期：2016/5/19
- * 描    述：
- * 修订历史：
- * ================================================
+ * author  : Black Chopper
+ * e-mail  : 4884280@qq.com
+ * github  : http://github.com/BlackChopper
+ * project : ImagePicker
  */
-public class ImagePreviewActivity extends ImagePreviewBaseActivity implements ImagePicker.OnImageSelectedListener, View.OnClickListener, CompoundButton.OnCheckedChangeListener {
-
-    public static final String ISORIGIN = "isOrigin";
-
-    private boolean isOrigin;                      //是否选中原图
-    private SuperCheckBox mCbCheck;                //是否选中当前图片的CheckBox
-    private SuperCheckBox mCbOrigin;               //原图
-    private Button mBtnOk;                         //确认图片的选择
-    private View bottomBar;
-    private View marginView;
-
+public class ImagePreviewActivity extends AbstractImagePreviewActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        isOrigin = getIntent().getBooleanExtra(ImagePreviewActivity.ISORIGIN, false);
-        imagePicker.addOnImageSelectedListener(this);
-        mBtnOk = (Button) findViewById(R.id.btn_ok);
-        mBtnOk.setVisibility(View.VISIBLE);
-        mBtnOk.setOnClickListener(this);
-
-        bottomBar = findViewById(R.id.bottom_bar);
-        bottomBar.setVisibility(View.VISIBLE);
-
-        mCbCheck = (SuperCheckBox) findViewById(R.id.cb_check);
-        mCbOrigin = (SuperCheckBox) findViewById(R.id.cb_origin);
-        marginView = findViewById(R.id.margin_bottom);
-        mCbOrigin.setText(getString(R.string.ip_origin));
-        mCbOrigin.setOnCheckedChangeListener(this);
-        mCbOrigin.setChecked(isOrigin);
-
-        //初始化当前页面的状态
-        onImageSelected(0, null, false);
-        ImageItem item = mImageItems.get(mCurrentPosition);
-        boolean isSelected = imagePicker.isSelect(item);
-        mTitleCount.setText(getString(R.string.ip_preview_image_count, mCurrentPosition + 1, mImageItems.size()));
-        mCbCheck.setChecked(isSelected);
-        //滑动ViewPager的时候，根据外界的数据改变当前的选中状态和当前的图片的位置描述文本
-        mViewPager.addOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-            @Override
-            public void onPageSelected(int position) {
-                mCurrentPosition = position;
-                ImageItem item = mImageItems.get(mCurrentPosition);
-                boolean isSelected = imagePicker.isSelect(item);
-                mCbCheck.setChecked(isSelected);
-                mTitleCount.setText(getString(R.string.ip_preview_image_count, mCurrentPosition + 1, mImageItems.size()));
-            }
-        });
-        //当点击当前选中按钮的时候，需要根据当前的选中状态添加和移除图片
-        mCbCheck.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ImageItem imageItem = mImageItems.get(mCurrentPosition);
-                int selectLimit = imagePicker.getSelectLimit();
-                if (mCbCheck.isChecked() && selectedImages.size() >= selectLimit) {
-                    Toast.makeText(ImagePreviewActivity.this, getString(R.string.ip_select_limit, selectLimit), Toast.LENGTH_SHORT).show();
-                    mCbCheck.setChecked(false);
-                } else {
-                    imagePicker.addSelectedImageItem(mCurrentPosition, imageItem, mCbCheck.isChecked());
-                }
-            }
-        });
-        NavigationBarChangeListener.with(this).setListener(new NavigationBarChangeListener.OnSoftInputStateChangeListener() {
-            @Override
-            public void onNavigationBarShow(int orientation, int height) {
-                marginView.setVisibility(View.VISIBLE);
-                ViewGroup.LayoutParams layoutParams = marginView.getLayoutParams();
-                if (layoutParams.height == 0) {
-                    layoutParams.height = Utils.getNavigationBarHeight(ImagePreviewActivity.this);
-                    marginView.requestLayout();
-                }
-            }
-
-            @Override
-            public void onNavigationBarHide(int orientation) {
-                marginView.setVisibility(View.GONE);
-            }
-        });
-        NavigationBarChangeListener.with(this, NavigationBarChangeListener.ORIENTATION_HORIZONTAL)
-                .setListener(new NavigationBarChangeListener.OnSoftInputStateChangeListener() {
-                    @Override
-                    public void onNavigationBarShow(int orientation, int height) {
-                        topBar.setPadding(0, 0, height, 0);
-                        bottomBar.setPadding(0, 0, height, 0);
-                    }
-
-                    @Override
-                    public void onNavigationBarHide(int orientation) {
-                        topBar.setPadding(0, 0, 0, 0);
-                        bottomBar.setPadding(0, 0, 0, 0);
-                    }
-                });
-    }
-
-
-
-    /**
-     * 图片添加成功后，修改当前图片的选中数量
-     * 当调用 addSelectedImageItem 或 deleteSelectedImageItem 都会触发当前回调
-     */
-    @Override
-    public void onImageSelected(int position, ImageItem item, boolean isAdd) {
-        if (imagePicker.getSelectImageCount() > 0) {
-            mBtnOk.setText(getString(R.string.ip_select_complete, imagePicker.getSelectImageCount(), imagePicker.getSelectLimit()));
-        } else {
-            mBtnOk.setText(getString(R.string.ip_complete));
-        }
-
-        if (mCbOrigin.isChecked()) {
-            long size = 0;
-            for (ImageItem imageItem : selectedImages)
-                size += imageItem.size;
-            String fileSize = Formatter.formatFileSize(this, size);
-            mCbOrigin.setText(getString(R.string.ip_origin_size, fileSize));
-        }
+    protected int attachImmersiveColorRes() {
+        return R.color.ip_color_primary_dark;
     }
 
     @Override
-    public void onClick(View v) {
-        int id = v.getId();
-        if (id == R.id.btn_ok) {
-            if (imagePicker.getSelectedImages().size() == 0) {
-                mCbCheck.setChecked(true);
-                ImageItem imageItem = mImageItems.get(mCurrentPosition);
-                imagePicker.addSelectedImageItem(mCurrentPosition, imageItem, mCbCheck.isChecked());
-            }
-            Intent intent = new Intent();
-            intent.putParcelableArrayListExtra(ImagePicker.EXTRA_RESULT_ITEMS, imagePicker.getSelectedImages());
-            setResult(ImagePicker.RESULT_CODE_ITEMS, intent);
-            finish();
-
-        } else if (id == R.id.iv_back) {
-            Intent intent = new Intent();
-            intent.putExtra(ImagePreviewActivity.ISORIGIN, isOrigin);
-            setResult(ImagePicker.RESULT_CODE_BACK, intent);
-            finish();
-        }
+    protected int attachTopBarRes() {
+        return R.id.top_bar;
     }
 
     @Override
-    public void onBackPressed() {
-        Intent intent = new Intent();
-        intent.putExtra(ImagePreviewActivity.ISORIGIN, isOrigin);
-        setResult(ImagePicker.RESULT_CODE_BACK, intent);
-        finish();
-        super.onBackPressed();
+    protected boolean attachImmersiveLightMode() {
+        return false;
     }
 
     @Override
-    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        int id = buttonView.getId();
-        if (id == R.id.cb_origin) {
-            if (isChecked) {
-                long size = 0;
-                for (ImageItem item : selectedImages)
-                    size += item.size;
-                String fileSize = Formatter.formatFileSize(this, size);
-                isOrigin = true;
-                mCbOrigin.setText(getString(R.string.ip_origin_size, fileSize));
-            } else {
-                isOrigin = false;
-                mCbOrigin.setText(getString(R.string.ip_origin));
-            }
-        }
+    protected int attachLayoutRes() {
+        return R.layout.activity_image_preview;
     }
 
     @Override
-    protected void onDestroy() {
-        imagePicker.removeOnImageSelectedListener(this);
-        super.onDestroy();
+    protected int attachViewPagerRes() {
+        return R.id.viewpager;
     }
 
-    /**
-     * 单击时，隐藏头和尾
-     */
     @Override
-    public void onImageSingleTap() {
-        if (topBar.getVisibility() == View.VISIBLE) {
-            topBar.setAnimation(AnimationUtils.loadAnimation(this, R.anim.top_out));
-            bottomBar.setAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_out));
-            topBar.setVisibility(View.GONE);
-            bottomBar.setVisibility(View.GONE);
-            tintManager.setStatusBarTintResource(Color.TRANSPARENT);//通知栏所需颜色
-        } else {
-            topBar.setAnimation(AnimationUtils.loadAnimation(this, R.anim.top_in));
-            bottomBar.setAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_in));
-            topBar.setVisibility(View.VISIBLE);
-            bottomBar.setVisibility(View.VISIBLE);
-            tintManager.setStatusBarTintResource(R.color.ip_color_primary_dark);//通知栏所需颜色
-        }
+    protected int attachBottomViewRes() {
+        return R.id.view_bottom;
+    }
+
+    @Override
+    protected int attachCheckOriginRes() {
+        return R.id.cb_origin;
+    }
+
+    @Override
+    protected int attachCheckRes() {
+        return R.id.cb_check;
+    }
+
+    @Override
+    protected int attachTitleRes() {
+        return R.id.tv_title;
+    }
+
+    @Override
+    protected int attachContentRes() {
+        return R.id.content;
+    }
+
+    @Override
+    protected int attachBottomBarRes() {
+        return R.id.bottom_bar;
+    }
+
+    @Override
+    protected int attachButtonOkRes() {
+        return R.id.btn_ok;
+    }
+
+    @Override
+    protected int attachButtonBackRes() {
+        return R.id.iv_back;
     }
 }
