85f06067980272ce6fd4c4844e963b92e4384bbb
Black Chopper <blackchopper@qq.com>
2018-07-07 11:56:59 +0800

    Update ImageCropActivity.java

--- library/src/main/java/com/blackchopper/imagepicker/ui/ImageCropActivity.java
+++ library/src/main/java/com/blackchopper/imagepicker/ui/ImageCropActivity.java
@@ -1,132 +1,51 @@
 package com.blackchopper.imagepicker.ui;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-import com.blackchopper.imagepicker.ImagePicker;
 import com.blackchopper.imagepicker.R;
-import com.blackchopper.imagepicker.bean.ImageItem;
-import com.blackchopper.imagepicker.util.BitmapUtil;
-import com.blackchopper.imagepicker.view.CropImageView;
-
-import java.io.File;
-import java.util.ArrayList;
 
 /**
- * ================================================
- * 作    者：jeasonlzy（廖子尧 Github地址：https://github.com/jeasonlzy0216
- * 版    本：1.0
- * 创建日期：2016/5/19
- * 描    述：
- * 修订历史：
- * ================================================
+ * author  : Black Chopper
+ * e-mail  : 4884280@qq.com
+ * github  : http://github.com/BlackChopper
+ * project : ImagePicker
  */
-public class ImageCropActivity extends ImageBaseActivity implements View.OnClickListener, CropImageView.OnBitmapSaveCompleteListener {
-
-    private CropImageView mCropImageView;
-    private Bitmap mBitmap;
-    private boolean mIsSaveRectangle;
-    private int mOutputX;
-    private int mOutputY;
-    private ArrayList<ImageItem> mImageItems;
-    private ImagePicker imagePicker;
-
+public class ImageCropActivity extends AbstractImageCropActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_image_crop);
-
-        imagePicker = ImagePicker.getInstance();
-
-        //初始化View
-        findViewById(R.id.iv_back).setOnClickListener(this);
-        Button btn_ok = (Button) findViewById(R.id.btn_ok);
-        btn_ok.setText(getString(R.string.ip_complete));
-        btn_ok.setOnClickListener(this);
-        TextView tv_des = (TextView) findViewById(R.id.tv_title);
-        tv_des.setText(getString(R.string.ip_photo_crop));
-        mCropImageView = (CropImageView) findViewById(R.id.cv_crop_image);
-        mCropImageView.setOnBitmapSaveCompleteListener(this);
-
-        //获取需要的参数
-        mOutputX = imagePicker.getOutPutX();
-        mOutputY = imagePicker.getOutPutY();
-        mIsSaveRectangle = imagePicker.isSaveRectangle();
-        mImageItems = imagePicker.getSelectedImages();
-        String imagePath = mImageItems.get(0).path;
-
-        mCropImageView.setFocusStyle(imagePicker.getStyle());
-        mCropImageView.setFocusWidth(imagePicker.getFocusWidth());
-        mCropImageView.setFocusHeight(imagePicker.getFocusHeight());
-
-        //缩放图片
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(imagePath, options);
-        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
-        options.inSampleSize = calculateInSampleSize(options, displayMetrics.widthPixels, displayMetrics.heightPixels);
-        options.inJustDecodeBounds = false;
-        mBitmap = BitmapFactory.decodeFile(imagePath, options);
-        mCropImageView.setImageBitmap(mCropImageView.rotate(mBitmap, BitmapUtil.getBitmapDegree(imagePath)));
-
-     }
-
-    public int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
-        int width = options.outWidth;
-        int height = options.outHeight;
-        int inSampleSize = 1;
-        if (height > reqHeight || width > reqWidth) {
-            if (width > height) {
-                inSampleSize = width / reqWidth;
-            } else {
-                inSampleSize = height / reqHeight;
-            }
-        }
-        return inSampleSize;
+    protected int attachButtonBackRes() {
+        return R.id.iv_back;
     }
 
     @Override
-    public void onClick(View v) {
-        int id = v.getId();
-        if (id == R.id.iv_back) {
-            setResult(RESULT_CANCELED);
-            finish();
-        } else if (id == R.id.btn_ok) {
-            mCropImageView.saveBitmapToFile(imagePicker.getCropCacheFolder(this), mOutputX, mOutputY, mIsSaveRectangle);
-        }
+    protected int attachButtonOkRes() {
+        return R.id.btn_ok;
     }
 
     @Override
-    public void onBitmapSaveSuccess(File file) {
-        mImageItems.remove(0);
-        ImageItem imageItem = new ImageItem();
-        imageItem.path = file.getAbsolutePath();
-        mImageItems.add(imageItem);
-
-        Intent intent = new Intent();
-        intent.putParcelableArrayListExtra(ImagePicker.EXTRA_RESULT_ITEMS, mImageItems);
-        setResult(ImagePicker.RESULT_CODE_ITEMS, intent);   //单选不需要裁剪，返回数据
-        finish();
+    protected int attachCropImageRes() {
+        return R.id.cv_crop_image;
     }
 
     @Override
-    public void onBitmapSaveError(File file) {
-
+    protected int attachTitleRes() {
+        return R.id.tv_title;
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mCropImageView.setOnBitmapSaveCompleteListener(null);
-        if (null != mBitmap && !mBitmap.isRecycled()) {
-            mBitmap.recycle();
-            mBitmap = null;
-        }
+    protected int attachImmersiveColorRes() {
+        return R.color.ip_color_primary_dark;
+    }
+
+    @Override
+    protected int attachTopBarRes() {
+        return R.id.top_bar;
+    }
+
+    @Override
+    protected boolean attachImmersiveLightMode() {
+        return false;
+    }
+
+    @Override
+    protected int attachLayoutRes() {
+        return R.layout.activity_image_crop;
     }
 }
