c51521b0433d88b2771b93c135f97bbc8fc1e620
Black Chopper <blackchopper@qq.com>
2018-07-07 11:55:53 +0800

    Update ImageBaseActivity.java

--- library/src/main/java/com/blackchopper/imagepicker/ui/ImageBaseActivity.java
+++ library/src/main/java/com/blackchopper/imagepicker/ui/ImageBaseActivity.java
@@ -1,18 +1,17 @@
 package com.blackchopper.imagepicker.ui;
 
-import android.annotation.TargetApi;
 import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Window;
+import android.support.v7.widget.Toolbar;
 import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.blackchopper.imagepicker.ImagePicker;
 import com.blackchopper.imagepicker.R;
+import com.blackchopper.imagepicker.util.ImmersiveHelper;
 import com.blackchopper.imagepicker.view.SystemBarTintManager;
 
 /**
@@ -28,26 +27,34 @@ public abstract class ImageBaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            setTranslucentStatus(true);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT && android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //设置虚拟导航栏为透明
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
         }
         tintManager = new SystemBarTintManager(this);
         tintManager.setStatusBarTintEnabled(true);
         tintManager.setStatusBarTintResource(R.color.ip_color_primary_dark);  //设置上方状态栏的颜色
+        setContentView(attachLayoutRes());
+        ImmersiveHelper.setOrChangeTranslucentColor(this, (Toolbar) findViewById(attachTopBarRes()), null, getResources().getColor(attachImmersiveColorRes()));
+        if (attachImmersiveLightMode())
+            ImmersiveHelper.setStatusBarLightMode(getWindow());
     }
 
-    @TargetApi(19)
-    private void setTranslucentStatus(boolean on) {
-        Window win = getWindow();
-        WindowManager.LayoutParams winParams = win.getAttributes();
-        final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
-        if (on) {
-            winParams.flags |= bits;
-        } else {
-            winParams.flags &= ~bits;
-        }
-        win.setAttributes(winParams);
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        ImagePicker.getInstance().saveInstanceState(outState);
     }
+
+    protected abstract int attachImmersiveColorRes();
+
+    protected abstract int attachTopBarRes();
+
+    protected abstract boolean attachImmersiveLightMode();
+
+    protected abstract int attachLayoutRes();
+
 
     public boolean checkPermission(@NonNull String permission) {
         return ActivityCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED;
@@ -61,11 +68,5 @@ public abstract class ImageBaseActivity extends AppCompatActivity {
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         ImagePicker.getInstance().restoreInstanceState(savedInstanceState);
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        ImagePicker.getInstance().saveInstanceState(outState);
     }
 }
