3ebbd84c8dccc944ccb8685df1e8c8ffc5fc3876
Black Chopper <blackchopper@qq.com>
2018-07-07 11:52:30 +0800

    Update ImageLoader.java

--- library/src/main/java/com/blackchopper/imagepicker/loader/ImageLoader.java
+++ library/src/main/java/com/blackchopper/imagepicker/loader/ImageLoader.java
@@ -1,24 +1,25 @@
 package com.blackchopper.imagepicker.loader;
 
 import android.app.Activity;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.widget.ImageView;
 
 import java.io.Serializable;
 
 /**
- * ================================================
- * 作    者：jeasonlzy（廖子尧 Github地址：https://github.com/jeasonlzy0216
- * 版    本：1.0
- * 创建日期：2016/5/19
- * 描    述：ImageLoader抽象类，外部需要实现这个类去加载图片， 尽力减少对第三方库的依赖，所以这么干了
- * 修订历史：
- * ================================================
+ * author  : Black Chopper
+ * e-mail  : 4884280@qq.com
+ * github  : http://github.com/BlackChopper
+ * project : ImagePicker
  */
-public interface ImageLoader extends Serializable {
+public interface ImageLoader extends Parcelable {
 
     void displayImage(Activity activity, String path, ImageView imageView, int width, int height);
 
     void displayImagePreview(Activity activity, String path, ImageView imageView, int width, int height);
 
     void clearMemoryCache();
+
+
 }
