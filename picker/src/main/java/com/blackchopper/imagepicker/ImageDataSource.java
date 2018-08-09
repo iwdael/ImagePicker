e028fb6aff4fcc51e03f742b4a779c87982e4435
Black Chopper <blackchopper@qq.com>
2018-08-09 15:01:56 +0800

    Update ImageDataSource.java

--- picker/src/main/java/com/blackchopper/imagepicker/ImageDataSource.java
+++ picker/src/main/java/com/blackchopper/imagepicker/ImageDataSource.java
@@ -1,6 +1,7 @@
 package com.blackchopper.imagepicker;
 
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.FragmentActivity;
@@ -25,7 +26,10 @@ public class ImageDataSource implements LoaderManager.LoaderCallbacks<Cursor> {
 
     public static final int LOADER_ALL = 0;         //加载所有图片
     public static final int LOADER_CATEGORY = 1;    //分类加载图片
-    private final String[] IMAGE_PROJECTION = {     //查询图片需要的数据列
+    public static final int LOADER_TYPE_IAMGE = 100;
+    public static final int LOADER_TYPE_VIDEO = 101;
+    private final String[] IMAGE_PROJECTION = {
+            //查询图片需要的数据列
             MediaStore.Images.Media.DISPLAY_NAME,   //图片的显示名称  aaa.jpg
             MediaStore.Images.Media.DATA,           //图片的真实路径  /storage/emulated/0/pp/downloader/wallpaper/aaa.jpg
             MediaStore.Images.Media.SIZE,           //图片的大小，long型  132492
@@ -34,14 +38,36 @@ public class ImageDataSource implements LoaderManager.LoaderCallbacks<Cursor> {
             MediaStore.Images.Media.MIME_TYPE,      //图片的类型     image/jpeg
             MediaStore.Images.Media.DATE_ADDED};    //图片被添加的时间，long型  1450518608
 
+
+    private final String[] VIDEO_PROJECTION = {     //查询图片需要的数据列
+            MediaStore.Video.Media.DISPLAY_NAME,   //图片的显示名称  aaa.jpg
+            MediaStore.Video.Media.DATA,           //图片的真实路径  /storage/emulated/0/pp/downloader/wallpaper/aaa.jpg
+            MediaStore.Video.Media.SIZE,           //图片的大小，long型  132492
+            MediaStore.Video.Media.WIDTH,          //图片的宽度，int型  1920
+            MediaStore.Video.Media.HEIGHT,         //图片的高度，int型  1080
+            MediaStore.Video.Media.MIME_TYPE,      //图片的类型     image/jpeg
+            MediaStore.Video.Media.DATE_ADDED,       //图片被添加的时间，long型  1450518608
+    };
+
+
     private FragmentActivity activity;
     private OnImagesLoadedListener loadedListener;                     //图片加载完成的回调接口
+    private String[] projections;
+    private Uri uri;
+    private int mainDirNameRes;
 
-
-    public ImageDataSource(FragmentActivity activity, String path, OnImagesLoadedListener loadedListener) {
+    public ImageDataSource(FragmentActivity activity, String path, int loadType, OnImagesLoadedListener loadedListener) {
         this.activity = activity;
         this.loadedListener = loadedListener;
-
+        if (loadType == LOADER_TYPE_IAMGE) {
+            projections = IMAGE_PROJECTION;
+            uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+            mainDirNameRes=R.string.ip_all_images;
+        } else {
+            projections = VIDEO_PROJECTION;
+            uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+            mainDirNameRes=R.string.all_video;
+        }
         LoaderManager loaderManager = activity.getSupportLoaderManager();
         if (path == null) {
             loaderManager.initLoader(LOADER_ALL, null, this);//加载所有的图片
@@ -58,10 +84,10 @@ public class ImageDataSource implements LoaderManager.LoaderCallbacks<Cursor> {
         CursorLoader cursorLoader = null;
         //扫描所有图片
         if (id == LOADER_ALL)
-            cursorLoader = new CursorLoader(activity, MediaStore.Images.Media.EXTERNAL_CONTENT_URI, IMAGE_PROJECTION, null, null, IMAGE_PROJECTION[6] + " DESC");
+            cursorLoader = new CursorLoader(activity, uri, projections, null, null, projections[6] + " DESC");
         //扫描某个图片文件夹
         if (id == LOADER_CATEGORY)
-            cursorLoader = new CursorLoader(activity, MediaStore.Images.Media.EXTERNAL_CONTENT_URI, IMAGE_PROJECTION, IMAGE_PROJECTION[1] + " like '%" + args.getString("path") + "%'", null, IMAGE_PROJECTION[6] + " DESC");
+            cursorLoader = new CursorLoader(activity, uri, projections, projections[1] + " like '%" + args.getString("path") + "%'", null, projections[6] + " DESC");
 
         return cursorLoader;
     }
@@ -73,19 +99,19 @@ public class ImageDataSource implements LoaderManager.LoaderCallbacks<Cursor> {
             ArrayList<ImageItem> allImages = new ArrayList<>();   //所有图片的集合,不分文件夹
             while (data.moveToNext()) {
                 //查询数据
-                String imageName = data.getString(data.getColumnIndexOrThrow(IMAGE_PROJECTION[0]));
-                String imagePath = data.getString(data.getColumnIndexOrThrow(IMAGE_PROJECTION[1]));
+                String imageName = data.getString(data.getColumnIndexOrThrow(projections[0]));
+                String imagePath = data.getString(data.getColumnIndexOrThrow(projections[1]));
 
                 File file = new File(imagePath);
                 if (!file.exists() || file.length() <= 0) {
                     continue;
                 }
 
-                long imageSize = data.getLong(data.getColumnIndexOrThrow(IMAGE_PROJECTION[2]));
-                int imageWidth = data.getInt(data.getColumnIndexOrThrow(IMAGE_PROJECTION[3]));
-                int imageHeight = data.getInt(data.getColumnIndexOrThrow(IMAGE_PROJECTION[4]));
-                String imageMimeType = data.getString(data.getColumnIndexOrThrow(IMAGE_PROJECTION[5]));
-                long imageAddTime = data.getLong(data.getColumnIndexOrThrow(IMAGE_PROJECTION[6]));
+                long imageSize = data.getLong(data.getColumnIndexOrThrow(projections[2]));
+                int imageWidth = data.getInt(data.getColumnIndexOrThrow(projections[3]));
+                int imageHeight = data.getInt(data.getColumnIndexOrThrow(projections[4]));
+                String imageMimeType = data.getString(data.getColumnIndexOrThrow(projections[5]));
+                long imageAddTime = data.getLong(data.getColumnIndexOrThrow(projections[6]));
                 //封装实体
                 ImageItem imageItem = new ImageItem();
                 imageItem.name = imageName;
@@ -117,7 +143,7 @@ public class ImageDataSource implements LoaderManager.LoaderCallbacks<Cursor> {
             if (data.getCount() > 0 && allImages.size() > 0) {
                 //构造所有图片的集合
                 ImageFolder allImagesFolder = new ImageFolder();
-                allImagesFolder.name = activity.getResources().getString(R.string.ip_all_images);
+                allImagesFolder.name = activity.getResources().getString(mainDirNameRes);
                 allImagesFolder.path = "/";
                 allImagesFolder.cover = allImages.get(0);
                 allImagesFolder.images = allImages;
@@ -125,7 +151,7 @@ public class ImageDataSource implements LoaderManager.LoaderCallbacks<Cursor> {
             }
         }
 
-        if (imageFolders.size() == 0 ) return;
+        if (imageFolders.size() == 0) return;
         //回调接口，通知图片数据准备完成
         ImagePicker.getInstance().imageFolders(imageFolders);
         loadedListener.onImagesLoaded(imageFolders);
@@ -133,7 +159,7 @@ public class ImageDataSource implements LoaderManager.LoaderCallbacks<Cursor> {
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
-     }
+    }
 
     /**
      * 所有图片加载完成的回调接口
