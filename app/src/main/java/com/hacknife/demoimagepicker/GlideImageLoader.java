57821b47e52aa4a92eb6f26a7ee2bcafe0cd6d2a
Hacknife <4884280@qq.com>
2018-10-12 23:33:39 +0800

    修改加载器

--- app/src/main/java/com/hacknife/demoimagepicker/GlideImageLoader.java
+++ app/src/main/java/com/hacknife/demoimagepicker/GlideImageLoader.java
@@ -19,27 +19,25 @@ import java.io.File;
 public class GlideImageLoader implements ImageLoader {
     @Override
     public void displayFileImage(ImageView imageView, String path) {
-        if (path.equals(imageView.getTag())) return;
+
         GlideApp.with(imageView.getContext())                             //配置上下文
                 .load(Uri.fromFile(new File(path)))      //设置图片路径(fix #8,文件名包含%符号 无法识别和显示)
                 .diskCacheStrategy(DiskCacheStrategy.ALL)//缓存全尺寸
                 .into(imageView);
-        imageView.setTag(path);
-    }
+     }
 
     @Override
     public void displayNetImage(ImageView imageView, String path) {
-        if (path.equals(imageView.getTag())) return;
+
         GlideApp.with(imageView.getContext()).load(path).into(imageView);
-        imageView.setTag(path);
+
     }
 
     @Override
     public void displayFullImage(ImageView imageView, String path) {
-        if (path.equals(imageView.getTag())) return;
 
         GlideApp.with(imageView.getContext()).load(path).into(imageView);
-        imageView.setTag(path);
+
     }
 
     @Override
